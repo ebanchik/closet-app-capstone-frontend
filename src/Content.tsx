9bb614ebe5c9715564017351406df0864c941230
ebanchik <ebanchik@gmail.com>
2024-02-05 16:55:17 -0500

    index action

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,10 +1,23 @@
+import axios from "axios"
+import { useState, useEffect } from "react";
 import { ItemsIndex } from "./ItemsIndex"
 
 export function Content() {
+  const [items, setItems] = useState([]);
+
+  const handleIndexItems = () => {
+    console.log("handleIndexItems");
+    axios.get("http://127.0.0.1:5000/items.json").then((response) => {
+      console.log(response.data);
+      setItems(response.data);
+    });
+  };
+
+  useEffect(handleIndexItems, []);
+     
   return (
     <main>
-      <h1>Welcome to React!</h1>
-      <ItemsIndex/>
+      <ItemsIndex items={items}/>
     </main>
   )
 }
--- src/ItemsIndex.tsx
+++ src/ItemsIndex.tsx
@@ -1,7 +1,31 @@
-export function ItemsIndex() {
+interface Item {
+  id: number;
+  name: string;
+  brand: string;
+  size: number;
+  color: string;
+  fit: string;
+  category_id: number;
+}
+
+interface ItemsIndexProps {
+  items: Item[];
+}
+
+export function ItemsIndex(props: ItemsIndexProps): JSX.Element {
   return (
     <div>
-      <h1>All photos</h1>
+      <h1>All items</h1>
+      {props.items.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          <p>Brand: {item.brand}</p>
+          <p>Size: {item.size}</p>
+          <p>Color: {item.color}</p>
+          <p>Fit: {item.fit}</p>
+          <p>Category: {item.category_id}</p>
+        </div>
+      ))}
     </div>
   );
 }
