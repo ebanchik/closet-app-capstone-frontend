979f3a066e4b269be233b8040cbaa2d03142e0f3
ebanchik <ebanchik@gmail.com>
2024-02-05 16:45:52 -0500

    late init

---
+++ src/Content.tsx
@@ -0,0 +1,10 @@
+import { ItemsIndex } from "./ItemsIndex"
+
+export function Content() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+      <ItemsIndex/>
+    </main>
+  )
+}
---
+++ src/ItemsIndex.tsx
@@ -0,0 +1,7 @@
+export function ItemsIndex() {
+  return (
+    <div>
+      <h1>All photos</h1>
+    </div>
+  );
+}
