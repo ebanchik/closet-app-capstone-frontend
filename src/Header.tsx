af8bacc5cc9bf004f958d8f6a6213b94d3d3b9b1
ebanchik <ebanchik@gmail.com>
2024-02-10 14:31:33 -0500

    update nav-bar

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,19 +3,16 @@ import { Link } from 'react-router-dom';
 export function Header() {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg custom-header-bg" style={{ height: '80px' }}>
-        <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg custom-header-bg" style={{ height: '80px', position: 'fixed', top: '0', zIndex:   1000 }}>
+        <div className="container-fluid d-flex justify-content-between align-items-center">
           <Link className="navbar-brand home-button" to="/">Home</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          {/* <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Holder</Link>
-              </li>
-            </ul>
-          </div> */}
+          <div className="navbar-buttons d-flex">
+            <Link className="navbar-brand login" to="/login">Login</Link>
+            <Link className="navbar-brand logout" to="/logout">Logout</Link>
+          </div>
         </div>
       </nav>
     </header>
