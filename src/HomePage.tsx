bd9fb841d0cc8752decefe8411a685f1d7e62e49
ebanchik <ebanchik@gmail.com>
2024-02-15 23:16:08 -0500

    add arrow, add conditional for + sign

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,11 +1,15 @@
-export const HomePage = () => {
-  return (
-    <div className="homepage">
-      <h1 className='homepage-text'>
-        Armoire
-      </h1>
-    </div>
-  );
-};
+// import arrow from "../assets/arrow.png"
 
-export default HomePage;
+// export const HomePage = () => {
+//   return (
+//     <div className="homepage">
+//       <h1 className='homepage-text'>
+//         Armoire
+//         <img src="/assets/arrow.png" alt="Arrow" style={{ width: "500px", height: "500px" }} />
+//       </h1>
+//         <h2>HI</h2>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
--- src/ItemsIndex.tsx
+++ src/ItemsIndex.tsx
@@ -46,49 +46,58 @@ export function ItemsIndex(props: ItemsIndexProps): JSX.Element {
 
 
   return (
-    <section className="index">
-      <div className="homepage">
-        <h1 className="homepage-text">Armoire</h1>
-      </div>
-      {isUserAuthenticated() ? (
-        <h1 className="index-header">CURRENT WARDROBE:</h1>
-      ) : (
-        <h1 className="index-header">Please Login</h1>
-      )}
-      <div className="container d-flex justify-content-center">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
-          {props.items.map((item) => {
-            console.log("Item:", item); // Log the item object
-            try {
-              const firstImageSrc = item.filenames && item.filenames.length > 0 ? `http://127.0.0.1:5000/uploads/${item.filenames[0]}` : undefined;
-
-              return (
-                <div className='cards' key={item.id}>
-                  <div className="card h-100 custom-card-font">
-                    {firstImageSrc && <img className="card-img-top" src={firstImageSrc} alt="Item Image" />}
-                    <div className="card-body">
-                      <h5 className="card-title card-title-custom"><Link to={`/item/${item.id}`}>{item.name}</Link></h5>
-                      <p className="card-text">
-                        {item.brand}<br />
-                        {/* Add other item details here */}
-                      </p>
-                      {/* <Link to={`/item/${item.id}`} className="btn btn-primary">View Details</Link> */}
+    <div className="container">
+      <section className="index">
+        <div className="homepage">
+          <h1 className="homepage-text">Armoire</h1>
+        </div>
+        <div className='center-container'>
+          <img src="/assets/arrow.png" alt="Arrow" className="arrow" style={{ width: "50px", height: "50px" }} />
+        </div>
+        {isUserAuthenticated() ? (
+          <>
+            <h1 className="index-header">CURRENT WARDROBE:</h1>
+          </>
+        ) : (
+          <h1 className="index-header">Please Login</h1>
+        )}
+        <div className="container d-flex justify-content-center">
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
+            {props.items.map((item) => {
+              console.log("Item:", item); // Log the item object
+              try {
+                const firstImageSrc = item.filenames && item.filenames.length > 0 ? `http://127.0.0.1:5000/uploads/${item.filenames[0]}` : undefined;
+  
+                return (
+                  <div className='cards' key={item.id}>
+                    <div className="card h-100 custom-card-font">
+                      {firstImageSrc && <img className="card-img-top" src={firstImageSrc} alt="Item Image" />}
+                      <div className="card-body">
+                        <h5 className="card-title card-title-custom"><Link to={`/item/${item.id}`}>{item.name}</Link></h5>
+                        <p className="card-text">
+                          {item.brand}<br />
+                          {/* Add other item details here */}
+                        </p>
+                        {/* <Link to={`/item/${item.id}`} className="btn btn-primary">View Details</Link> */}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            } catch (error) {
-              console.error("Error rendering item:", error); // Log any rendering errors
-              return null; // Return null if there's an error
-            }
-          })}
+                );
+              } catch (error) {
+                console.error("Error rendering item:", error); // Log any rendering errors
+                return null; // Return null if there's an error
+              }
+            })}
+          </div>
         </div>
-      </div>
-      <div className="container d-flex justify-content-center mt-4">
-        <Link to="/new-item" className="add-button-2">+</Link>
-      </div>
-    </section>
+      </section>
+      {isUserAuthenticated() && (
+        <div className="container d-flex justify-content-center mt-4">
+          <Link to="/new-item" className="add-button-2">+</Link>
+        </div>
+      )}
+    </div>
   );
-}
+      }  
 
 export default ItemsIndex;
