6ed54c0266eb133311833aa5f8d12b4e4e60e6fc
ebanchik <ebanchik@gmail.com>
2024-02-25 13:58:56 -0800

    fix image responiveness

--- src/Header.tsx
+++ src/Header.tsx
@@ -33,9 +33,6 @@ export function Header({ onSearch }: { onSearch: (searchTerm: string) => void })
       <nav className="navbar navbar-expand-lg custom-header-bg" style={{ height: '80px', position: 'fixed', top: '0', zIndex:   1000 }}>
         <div className="container-fluid d-flex justify-content-between align-items-center">
           <Link className="navbar-brand home-button" to="/">HOME</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="navbar-buttons d-flex">
             <Link className="navbar-brand login" to="/login">LOGIN</Link>
             <button className="navbar-brand logout" onClick={handleLogout}>LOGOUT</button>
